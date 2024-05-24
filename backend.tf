00eb3f199c1d0da5061067cdd2c865130f9d32ee
Omkar-doc <165380737+Omkar-doc@users.noreply.github.com>
2024-05-24 17:48:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "omabuck009"
+    bucket = "mydev-tf-state-bucket-project-terraform"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
